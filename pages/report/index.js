2deeb1c493fd99edf09867f667c5f8e837a0d4b6
Garrett Amini <garrett@jet.com>
2017-03-06 18:08:04 -0500

    adding sources

--- pages/report/index.js
+++ pages/report/index.js
@@ -47,6 +47,18 @@ class ReportPage extends React.Component {
                 <h2 className={s.harmTitle}>{harm.group}</h2>
                 <h3 className={s.harmTerm}>{harm.term}</h3>
                 {harm.description.split('\n').map((item, key) => <p key={`harm_${key}`}>{item}</p>)}
+                {}
+                <h4>Sources:</h4>
+                <ul className={s.harmSources}>
+                  {harm.sources.map((source, i) =>
+                    <li key={`${harm.id}-source-${i}`} className={s.harmSource}>
+                      <h5 className={s.harmSourceTitle}>
+                        <a href={source.link}>{source.title}</a>
+                      </h5>
+                      <h6 className={s.harmSourceName}>{source.name}</h6>
+                    </li>
+                  )}
+                </ul>
               </div>
             </li>
           )}
