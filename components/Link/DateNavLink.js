7a679cf714838366a0ee9c15e9cc1a4407465731
Garrett Amini <garrett@jet.com>
2017-03-06 16:22:17 -0500

    styling

--- components/Link/DateNavLink.js
+++ components/Link/DateNavLink.js
@@ -15,11 +15,18 @@ class DateNavLink extends React.Component {
 		console.log('rendering datenavlink')
 		let date = moment(this.props.date);
 		let type = this.props.type;
+		let arrow;
+
+		if (this.props.type === 'prev')
+			arrow = <img className={`${s.arrow} ${s.arrowPrev}`} src='/assets/img/arrow_right.svg' />;
+		else if (this.props.type === 'next')
+			arrow = <img className={`${s.arrow} ${s.arrowNext}`} src='/assets/img/arrow_left.svg' />;
 
 		return (
 			<Link className={`${s.link} ${s[type]}`} to={`/d/${date.format('YYYYMMDD')}`}>
 				<span className={s.date}>{date.format('DD MMM')}</span>
 				<span className={s.year}>{date.format('YYYY')}</span>
+				{arrow}
 			</Link>
 		)
 	}
